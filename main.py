33f423dc365e11541ff7de757e3861b26300f354
Jonathan Dickinson <jon.dickinson17@gmail.com>
2022-12-27 16:03:54 -0800

    Connect calendly link to the sidebar

--- main.py
+++ main.py
@@ -1,69 +1,15 @@
 import streamlit as st
-import pandas as pd
 from PIL import Image
+from streamlit_extras.colored_header import colored_header
+from markdownlit import mdlit
+from annotated_text import annotated_text
 
 st.set_page_config(
-    page_title="CWU Fitlab"
+    page_title="CWU Fitlab",
+    layout='wide'
 )
 
-# -- CSS styling --
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-# -- Header --
-st.title("Welcome to CWU Fitlab!")
-
-with st.container():
-    fitlab_img = Image.open('img/fitlab-img.png')
-    st.image(fitlab_img, caption="Students during Fitlab")
-    st.header('Overview')
-    st.subheader('What is fit lab? Who can participate? How much does it cost?')
-    st.write(
-        '''
-        - CWU Fit Lab is a comprehensive fitness assessment experience provided by trained CWU students pursuing a degree in Exercise Science. 
-        We offer two services: Full fitness assessments and body composition only assessments.  
-        - Cost: These assessments are free of charge and available to anyone 18 years of age and older (community members, students, faculty, etc.).  
-        
-        Please note that proof of COVID-19 vaccination is required at the time of scheduling
-        '''
-    )
-
-    st.header("FAQ")
-    with st.expander("What exactly does each fitness evaluation (body composition vs. full fitness) assess?"):
-        st.write(
-            '''
-            - Full fitness assessment: Includes a series of physical tests that assess five components of fitness: cardiorespiratory fitness (how well your heart and lungs can supply oxygen to your working muscles during exercise), body composition (proportion of fat versus fat free mass and associated health risk), flexibility, muscular strength and endurance. 
-            - Body composition only assessment: The body composition only service focuses on two measurements for body composition (underwater weighing and three or seven site skinfold) with comparison of individual results to age-related norms and assessment of associated health risk. 
-            
-            - How long does it take? The full fitness assessment takes about 75 minutes.  The body composition only option takes approximately 40 minutes.
-            When can I participate this quarter? Times and days vary based on the academic quarter.  
-            '''
-        )
-    with st.expander("What can participants do with their assessment report?"):
-        st.write(
-            """
-            A full fitness assessment will evaluate an individual's overall fitness level.
-            Typically, a more fit individual has a lower risk for developing disease or disability, 
-            an improved quality of life, and the capacity to complete activities of daily living 
-            with greater ease.  The results from a full fitness assessment can help to establish 
-            a baseline (determining the student's current fitness level and a starting point for 
-            exercise programming) and if repeated after an exercise training program, can show progress 
-            made toward health or fitness related goals. 
-            """
-        )
-
-    with st.expander("How can athletes specifically benefit from taking the evaluation?"):
-        st.write(
-            """
-            For athletes, a fitness evaluation serves to measure their readiness for sport-related 
-            activities (for example, a higher cardiorespiratory fitness level may translate to 
-            improved cardiovascular sport performance like running or cycling) and can also identify 
-            areas in need of improvement to optimize sport performance. 
-            """
-        )
 with st.sidebar:
-    st.title('Fitlab')
-    st.markdown("Click [here](https://calendly.com/) to schedule an assessment or come to Heath Sciences 117 during open hours!")
     st.subheader("Hours of Operation")
     st.write(
         """
@@ -76,5 +22,86 @@ with st.sidebar:
         """
         - Central Washington University Health Sciences Building
         - Room 117
+        """
+    )
+    mdlit (
+        """Check out our @(calendar)(https://calendly.com/cwu-fitlab/assessment) to schedule an assessment or come to [red]Heath 
+        Sciences 117[/red] during open hours! 
+        If you have any further questions feel free to email **[blue]fitlab.cwu.edu[/blue]**.
+        """
+    )
+
+
+# -- Header --
+st.title("Welcome to CWU Fitlab!")
+
+# col_left, col_right = st.columns(2)
+fitlab_img = Image.open('img/fitlab-img.png')
+st.image(fitlab_img, caption="Students during Fitlab")
+colored_header(
+    label="Overview",
+    description="Learn more about Fitlab",
+    color_name="red-70",
+)
+st.subheader('What is fit lab? Who can participate? How much does it cost?')
+st.write(
+    '''
+    - CWU Fit Lab is a **comprehensive fitness assessment** experience provided by trained CWU students pursuing a degree in Exercise Science.   
+    - Cost: These assessments are free of charge and available to anyone 18 years of age and older (community members, students, faculty, etc.).  
+        
+    Please note that proof of COVID-19 vaccination is required at the time of scheduling
+    '''
+)
+
+annotated_text(
+    "We offer two services: ",
+    ("full fitness assessments", "~75min", "#faa"),
+    " and ",
+    ("body composition only", "~45min", "#faa"),
+    " assessments ",
+)
+
+colored_header(
+    label="FAQ",
+    description="Frequently Asked Questions",
+    color_name="red-70",
+)
+with st.expander("What exactly does each fitness evaluation (body composition vs. full fitness) assess?"):
+    st.write(
+        '''
+        - **Full fitness assessment**: Includes a series of physical tests that assess five components of fitness:
+            - Cardiorespiratory fitness (how well your heart and lungs can supply oxygen to your working muscles during exercise)
+            - Body composition (proportion of fat versus fat free mass and associated health risk)
+            - Flexibility
+            - Muscular strength
+            - Endurance. 
+        - **Body composition only assessment**: The body composition only service focuses on two measurements for body composition (underwater weighing and three or seven site skinfold) with comparison of individual results to age-related norms and assessment of associated health risk. 
+            
+        - How long does it take? The full fitness assessment takes about 75 minutes.  The body composition only option takes approximately 40 minutes.
+        When can I participate this quarter? Times and days vary based on the academic quarter.  
+        '''
+    )
+with st.expander("What can participants do with their assessment report?"):
+    st.write(
+        """
+        A full fitness assessment will evaluate an individual's overall fitness level.
+        Typically, a more fit individual has a lower risk for developing disease or disability, 
+        an improved quality of life, and the capacity to complete activities of daily living 
+        with greater ease.  
+        
+        The results from a full fitness assessment can help to establish 
+        a baseline (determining the student's current fitness level and a starting point for 
+        exercise programming) and if repeated after an exercise training program, can show progress 
+        made toward health or fitness related goals. 
+        """
+    )
+
+with st.expander("How can athletes specifically benefit from taking the evaluation?"):
+    st.write(
+        """
+        For athletes, a fitness evaluation serves to measure their readiness for sport-related 
+        activities (for example, a higher cardiorespiratory fitness level may translate to 
+        improved cardiovascular sport performance like running or cycling) and can also identify 
+        areas in need of improvement to optimize sport performance. 
         """
     )
